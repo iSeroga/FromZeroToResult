7508c04f340bd1f79ae616ae0170bacd751afa30
iSeroga <tedb9556@gmail.com>
2022-10-31 22:42:47 +0200

    getter and seter new fuctional

---
+++ lesson99/script.js
@@ -0,0 +1,16 @@
+'use strict'
+
+const person = {
+ name:"alex",
+ age: 25,
+
+ get userAge(){
+    return this.age;
+ },
+
+ set userAge(num){
+    this.age = num;
+ }
+};
+console.log(person.userAge = 30);
+console.log(person.userAge);
