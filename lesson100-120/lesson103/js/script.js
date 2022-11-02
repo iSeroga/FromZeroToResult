cb7d3c704c54a7fe5fd636039a2ea54525113dc5
iSeroga <tedb9556@gmail.com>
2022-11-02 12:02:18 +0200

    Import and export specifics work with webpacker compilator

---
+++ lesson100-120/lesson103/js/script.js
@@ -0,0 +1,12 @@
+// import { one, two } from "./main";
+// import { one as first } from "./main"; // this way you can change name of varieble or eport file
+ import  * as data from "./main.js";
+import sayHi from "./main.js";
+
+// console.log(`${one} and ${two}`);
+
+ console.log(`${data.one} and ${data.two}`);
+sayHi();
+
+//  we importing object in fact 
+// onlt one default eport
