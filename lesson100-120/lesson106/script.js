10e8eee92771e3e8c55620d4282d976cedfcecc4
iSeroga <tedb9556@gmail.com>
2022-11-05 05:39:50 +0200

    Few lessons with theory how to catch problems

---
+++ lesson100-120/lesson106/script.js
@@ -0,0 +1,16 @@
+'use strict'
+// try {
+//     console.log('normal');
+//     console.log(a);
+//     console.log('result');
+
+// } catch(e) {
+//     console.log('error')
+// } finally {
+
+// }
+
+// console.log('still normal');
+
+
+// HEre was more theory
