05a9c8e05f3e502d1588f92458ea90c88dabfeef
iSeroga <tedb9556@gmail.com>
2022-11-06 15:18:12 +0200

    Lesson 110 is about jQuery libraru first step and lesson 111 is about function generator

---
+++ lesson100-120/lesson110/script.js
@@ -0,0 +1,25 @@
+import $ from 'jquery';
+
+// const btn = $('#btn');
+
+// console.log(btn);
+
+$(document).ready(function () {
+    $('.list-item:first').hover(function () {
+        $(this).toggleClass('active');
+    });
+
+    $('.list-item:eq(2)').on('click', function() {
+        $('.image:even').fadeToggle();
+    });
+
+    $('.list-item:eq(4)').on('click', function() {
+        $('.image:odd').animate({
+            opacity:"toggle",
+            height: "toggle"
+        }, 2000)
+    })
+
+});
+//
+//
