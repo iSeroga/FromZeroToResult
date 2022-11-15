308cb2f5d7fbd7f400aa71fcbb337547dd97cf16
iSeroga <tedb9556@gmail.com>
2022-11-15 18:23:47 +0200

    Firs react project. Created shablon.

---
+++ lesson121-140/lesson124/big-project/src/components/app-info/app-info.js
@@ -0,0 +1,14 @@
+import './app-info.css';
+
+
+const AppInfo = () => {
+    return (
+        <div className="app-info">
+            <h1>Кількість працівників в компанії</h1>
+            <h2>Загальна кількість працівників</h2>
+            <h2> Премію отримують: </h2>
+        </div>
+    );
+}
+
+export default AppInfo
