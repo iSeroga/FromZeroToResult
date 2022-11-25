99f76a4a4b5bcd977de205a8ac3bf5902bfee069
iSeroga <tedb9556@gmail.com>
2022-11-25 11:59:29 +0200

    Styled components. Css in Js

--- lesson121-140/lesson122/from-lesson-122/src/App.js
+++ lesson121-140/lesson122/from-lesson-122/src/App.js
@@ -1,6 +1,35 @@
-import {Component, Fragment} from 'react';
+import {Component} from 'react';
+import styled from 'styled-components';
 
 import './App.css';
+
+const EmpItem = styled.div`
+  padding:20px ;
+  margin-bottom: 15px;
+  border-radius: 5px;
+  box-shadow: 5px 5px 10px rgba(0,0,0, .2);
+    a {
+      display: block;
+      margin: 10px 0;
+      color: ${props => props.active ? 'orange' : 'black'};
+    }
+    input {
+      display: block;
+      margin: 10px 0;
+    }
+`;
+
+const Header = styled.h2`
+  font-size: 22px;
+`;
+
+ export const Button = styled.button`
+  display: block ;
+  padding:5px 15px;
+  background-color: gold;
+  border: 1px solid rgba(0, 0,0, .2);
+  box-shadow: 5px 5px 10px rgba(0, 0,0, .2);
+`;
 
 class WhoAmI extends Component {
   constructor(props) {
@@ -29,26 +58,30 @@ class WhoAmI extends Component {
       const {name, surname, link} = this.props;
       const {position, years} = this.state
       return (
-        <Fragment>
-          <button onClick={this.nextYear}>{this.state.text}</button>
-          <h1>My name is: {name}, surname - {surname}, age - {years}, position - {position}</h1>
+        <EmpItem active>
+          <Button onClick={this.nextYear}>{this.state.text}</Button>
+          <Header>My name is: {name}, surname - {surname}, age - {years}, position - {position}</Header>
           <a href={link}>My profile</a>
           <form>
             <span>Введіть посаду</span>
             <input type='text' onChange={(e) => this.commitInputChanges(e, "some color")}/>
           </form>
-        </Fragment>
+        </EmpItem>
       );
   }
 
 }
+const Wrapper = styled.div`
+  width: 600px;
+  margin: 80px auto 0 auto;
+`;
 
 function App() {
   return (
-    <div className="App">
+    <Wrapper>
       <WhoAmI name= "John" surname="Smith" link="facebook.com"/>
-      <WhoAmI name="alex" surname="Johnson" link="pornhub.com"/>
-    </div>
+      <WhoAmI name="alex" surname="Johnson" link="normlink.com"/>
+    </Wrapper>
   );
 }
 
--- lesson121-140/lesson122/from-lesson-122/src/index.js
+++ lesson121-140/lesson122/from-lesson-122/src/index.js
@@ -1,12 +1,24 @@
-import React from 'react';
+import React, { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { Button } from './App';
+import styled from 'styled-components';
+
+const BigButton = styled(Button)`
+  margin:0 auto;
+  width: 245px;
+  text-align: center;
+`;
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+<StrictMode>
   <App/>
+  <BigButton as="a">Send report</BigButton>
+  </StrictMode>
 );
+
 
 
 
