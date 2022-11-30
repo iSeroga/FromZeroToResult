228293bf3ddbcdd45cb600c52e2cdbe1171e08b2
iSeroga <tedb9556@gmail.com>
2022-11-30 21:33:08 +0200

    First try of react-pet-project: Failure

---
+++ skill-test-project/coffe-shop/src/components/app/app.js
@@ -0,0 +1,16 @@
+import { Component } from 'react';
+
+import './app.css'
+import '../header/header'
+import Header from '../header/header';
+
+class App extends Component {
+    render() {
+
+        return(
+            <Header/>
+        )
+    }
+}
+
+export default App;
---
+++ skill-test-project/coffe-shop/src/components/header/header.js
@@ -0,0 +1,13 @@
+import './header.css';
+
+const Header = () => {
+    return(
+        <div>
+            <span>Coffee house</span>
+            <span>Our coffee</span>
+            <span></span>
+        </div>
+    )
+}
+
+export default Header
