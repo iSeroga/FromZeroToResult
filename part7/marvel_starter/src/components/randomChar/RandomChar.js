325389693032ebc5d599f73d5078eb1e2a460080
iSeroga <tedb9556@gmail.com>
2022-12-11 23:31:40 +0200

    I am back. Every 3 sends random charackter + random charackter on click try it + 9 charakcters from array(server) on page in charList Struckture of Reack >>>  COnstructor> render> react updateDOm> Component Did Mount\componentDidUpdate

--- part7/marvel_starter/src/components/randomChar/RandomChar.js
+++ part7/marvel_starter/src/components/randomChar/RandomChar.js
@@ -8,10 +8,6 @@ import './randomChar.scss';
 import mjolnir from '../../resources/img/mjolnir.png';
 
 class RandomChar extends Component {
-    constructor(props){
-        super(props);
-        this.updateChar();
-    }
     state = {
         char: {},
         loading: true,
@@ -25,6 +21,14 @@ class RandomChar extends Component {
     }
 
     marvelService = new MarvelService();
+    componentDidMount() {
+        this.updateChar();
+        this.timerId = setInterval(this.updateChar, 3000);
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.timerId);
+    }
 
     onCharLoaded = (char) => {
         this.setState({
@@ -59,7 +63,7 @@ class RandomChar extends Component {
                 <p className="randomchar__title">
                     Or choose another one
                 </p>
-                <button className="button button__main">
+                <button className="button button__main" onClick={this.updateChar}>
                     <div className="inner">try it</div>
                 </button>
                 <img src={mjolnir} alt="mjolnir" className="randomchar__decoration"/>
@@ -71,10 +75,14 @@ class RandomChar extends Component {
 const View = ({char}) => {
 
     const {name, description, thumbnail, homepage, wiki} = char;
+    let imgStyle = {'objectFit' : "cover"};
+    if(thumbnail ==='http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg'){
+        imgStyle = {'objectFit' : "contain"};
+    }
 
     return(
             <div className="randomchar__block">
-            <img src={thumbnail} alt="Random character" className="randomchar__img"/>
+            <img src={thumbnail} alt="Random character" className="randomchar__img" style={imgStyle}/>
                 <div className="randomchar__info">
                     <p className="randomchar__name">{name}</p>
                     <p className="randomchar__descr">
@@ -94,3 +102,12 @@ const View = ({char}) => {
 }
 
 export default RandomChar;
+
+//home WOrk 
+
+
+// Try It Коли клікає має завантажуватись новий перс  ↑
+// Змінити заглужку з cover (css) to contain (object fit) ↑
+// render персонажів на сторінці (9) 
+// Запрос на сервер Отримати 9 персів  побудувати інтерфейс 
+// унікальні айді
