a01b0ba50d8ac812c320276094cc0f1fb3285247
iSeroga <tedb9556@gmail.com>
2022-12-17 09:10:30 +0200

    I don	 know what is heppen but Here we have ref/ Site little broken but general react is workong

--- lesson121-140/lesson121/my-app/src/App.js
+++ lesson121-140/lesson121/my-app/src/App.js
@@ -1,17 +1,56 @@
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import {Container} from 'react-bootstrap';
 import './App.css';
 
+class Form extends Component {
+    constructor(props) {
+        super(props);
+        this.myRef = React.createRef();
+    }
+
+    componentDidMount() {
+      this.myRef.current.doSmth();
+    }
+
+    render() {
+        return (
+            <Container>
+                <form className="w-50 border mt-5 p-3 m-auto">
+                    <div className="mb-3">
+                        <label htmlFor="exampleFormControlInput1" className="form-label">Email address</label>
+                        <TextInput ref={this.myRef}/>
+                    </div>
+                    <div className="mb-3">
+                        <label htmlFor="exampleFormControlTextarea1" className="form-label">Example textarea</label>
+                        <textarea className="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                    </div>
+                </form>
+            </Container>
+        )
+    }
+}
+
+class TextInput extends Component {
+  doSmth = () => {
+    console.log('piska');
+  }
+  render()
+  {
+    return(
+      <input 
+       type="email"
+        className="form-control" 
+        id="exampleFormControlInput1" 
+        placeholder="name@example.com"/>
+
+    )
+  }
+}
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-      </header>
-    </div>
-  );
+    return (
+        <Form/>
+    );
 }
 
 export default App;
