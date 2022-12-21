4bde4046d03413bf396887da01f1df2de1c4f01f
iSeroga <tedb9556@gmail.com>
2022-12-21 09:30:01 +0200

    deploy to gh-pages

--- part7/marvel_starter/src/components/charList/CharList.js
+++ part7/marvel_starter/src/components/charList/CharList.js
@@ -1,4 +1,6 @@
 import { Component } from 'react';
+import PropTypes from 'prop-types'
+
 import Spinner from '../spinner/Spinner';
 import ErrorMessage from '../errorMessage/ErrorMessage';
 import MarvelService from '../../services/MarvelService';
@@ -12,6 +14,7 @@ class CharList extends Component {
         error:false,
         newItemLoading: false,
         offset: 210,
+        charEnded: false
     }
 
     marvelService = new MarvelService();
@@ -55,8 +58,23 @@ class CharList extends Component {
         })
     }
 
+    itemsRefs = [];
+
+    setRef = (ref) => {
+        this.itemsRefs.push(ref);
+    }
+
+    focusOnItem = (id) => {
+
+        this.itemsRefs.forEach(item => item.classList.remove("char__item_selected"));
+        this.itemsRefs[id].classList.add('char__item_selected');
+        this.itemsRefs[id].focus();
+    }
+
+
+
     renderItems(arr) {
-        const items = arr.map((item) => {
+        const items = arr.map((item, i) => {
             let imgStyle = {'objectFit' : 'cover'};
             if(item.thumbnail === 'http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg') {
                 imgStyle = {'objectFit' : 'unset'};
@@ -66,8 +84,21 @@ class CharList extends Component {
         return(
             <li
              className="char__item"
+              tabIndex={0}
+              ref={this.setRef}
               key={item.id}
-              onClick={() => this.props.onCharSelected(item.id)}>
+              onClick={() => {
+
+                        this.props.onCharSelected(item.id);
+                        this.focusOnItem(i)
+                        }}>
+
+                        onKeyPress={(e)=>{
+                            if(e.key ===" " || e.key === "Enter"){
+                                this.props.onCharSelected(item.id);
+                                this.focusOnItem(i)
+                            }
+                        }}
                         <img src={item.thumbnail} alt={item.name} style={imgStyle}/>
                         <div className="char__name">{item.name}</div>
                     </li>
@@ -109,3 +140,7 @@ class CharList extends Component {
 }
 
 export default CharList;
+// focus
+
+// tabIndex
+// if focus doesn`t work check per console.log dir
